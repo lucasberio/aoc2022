0d94c17879284cf6e3e494664751f60944938be4
lucasberio <lucasberio815@gmail.com>
2022-12-01 14:26:57 -0600

    started pt 2

--- Day01/Day01.java
+++ Day01/Day01.java
@@ -49,11 +49,51 @@ public class Day01 {
 
 
     public static void part2() {
+        Scanner in;
+        try {
+            in = new Scanner(new File(DATA_FILE));
+
+        } catch (Exception e) {
+            System.out.println(e);
+            return;
+        }
+
+        
+        
+        int mostCalories = 0;
+
+        while (in.hasNext()) {
+            String line = in.nextLine();
+            int calories  = 0;
+            
+            //get calories not separated by a space
+            while(!line.equals("")) {
+                calories += Integer.parseInt(line);
+
+                if(in.hasNext()){
+                    line = in.nextLine();
+                }
+
+                else {
+                    break;
+                }
+
+            }
+
+            if (calories > mostCalories) {
+                mostCalories = calories;
+            }
+
+
+
+        }
+
+        System.out.println(mostCalories);
         
     }
 
     public static void main(String[] args) {
-        part1();
+        part2();
 
     }
 
