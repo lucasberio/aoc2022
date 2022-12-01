487b24c80105f29dbf16b81076fafd3a5f4cd162
lucasberio <lucasberio815@gmail.com>
2022-12-01 14:18:26 -0600

    Part 1 Day 1 complete

--- Day01/Day01.java
+++ Day01/Day01.java
@@ -1,12 +1,60 @@
+import java.util.Scanner;
+import java.io.File;
+
 public class Day01 {
 
+    private final static String DATA_FILE = "input.txt";
+
     public static void part1() {
+        Scanner in;
+        try {
+            in = new Scanner(new File(DATA_FILE));
+
+        } catch (Exception e) {
+            System.out.println(e);
+            return;
+        }
+        
+        int mostCalories = 0;
+
+        while (in.hasNext()) {
+            String line = in.nextLine();
+            int calories  = 0;
+            
+            //get calories not separated by a space
+            while(!line.equals("")) {
+                calories += Integer.parseInt(line);
+
+                if(in.hasNext()){
+                    line = in.nextLine();
+                }
+
+                else {
+                    break;
+                }
+
+            }
+
+            if (calories > mostCalories) {
+                mostCalories = calories;
+            }
+
+
+
+        }
+
+        System.out.println(mostCalories);
 
     }
 
 
     public static void part2() {
         
+    }
+
+    public static void main(String[] args) {
+        part1();
+
     }
 
 
