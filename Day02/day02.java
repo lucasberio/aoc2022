d667bf8a550c25714183abb458e0547bfead02c1
lucasberio <lucasberio815@gmail.com>
2022-12-05 14:07:17 -0600

    finished pt 2

--- Day02/day02.java
+++ Day02/day02.java
@@ -24,6 +24,24 @@ class Day02 {
 
     }
 
+    public static int evaluate2(String strategy) {
+        int score = 0;
+        String[] choices = strategy.split(" ");
+        int otherChoice = choices[0].compareTo("A");
+        int result = choices[1].compareTo("X");
+        switch (result) {
+            case 0: score += Math.floorMod(otherChoice - 1, 3) + 1 ; break;
+            case 1: score += (otherChoice + 1) + 3; break;
+            case 2: score += Math.floorMod(otherChoice + 1, 3) + 7 ; break;   
+        }
+
+
+        return score;
+
+
+
+    }
+
     public static void part1() throws IOException{
         Scanner in = new Scanner(new File("input.txt"));
         int total = 0;
@@ -37,13 +55,22 @@ class Day02 {
 
     }
 
-    public static void part2(){
+    public static void part2() throws IOException{
+        Scanner in = new Scanner(new File("input.txt"));
+        int total = 0;
+
+        while(in.hasNext()){
+            String line = in.nextLine();
+            total += evaluate2(line);
+
+        }
+        System.out.println(total);
 
     }
 
     public static void main(String...args) {
         try{
-            part1();
+            part2();
         }
         catch ( Exception e) {
             System.out.println(e);
