8bdf420e2960e82b4bd1444abfcbfc42ffe6bbd1
lucasberio <lucasberio815@gmail.com>
2022-12-07 14:26:14 -0600

    day5 pt 2

--- Day03/Day03.java
+++ Day03/Day03.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import java.util.*;
 import java.io.*;
 
@@ -27,6 +28,34 @@ public class Day03 {
                 if(secondHalf.indexOf(c) != -1) {
                     // turn c into 1-26 or 27-52
                     //System.out.println(c + " " + decode(c));
+=======
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.File;
+
+public class Day03{
+
+
+    public static int decode(String c) {
+        if(c.compareTo("a") >=0){
+            return c.compareTo("a") + 1;
+        }else{
+            return c.compareTo("A") + 27;
+        }
+
+    }
+    public static void part1() throws IOException{
+        int total = 0;
+        Scanner in = new Scanner(new File("input.txt"));
+        while(in.hasNext()){
+            String line = in.nextLine();
+            int half = line.length()/2;
+            String firstHalf = line.substring(0, half);
+            String secondHalf = line.substring(half);
+            for(int i = 0; i < half; i++){
+                String c = firstHalf.substring(i, i+1);
+                if(secondHalf.indexOf(c) != -1) {
+>>>>>>> 9cf31a7143bce6e958209e2d5ac833ac7694a671
                     total += decode(c);
                     break;
                 }
@@ -34,6 +63,7 @@ public class Day03 {
             }
         }
         System.out.println(total);
+<<<<<<< HEAD
     }
 
     public static void part2() throws IOException {
@@ -62,9 +92,28 @@ public class Day03 {
         try {
             part2();
         } catch (Exception e) {
+=======
+   
+
+    }
+
+    public static void part2() throws IOException {
+       
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            part1();
+        } catch (Exception e){
+>>>>>>> 9cf31a7143bce6e958209e2d5ac833ac7694a671
             System.out.println(e);
         }
     }
 
+<<<<<<< HEAD
 
 }    
+=======
+}
+>>>>>>> 9cf31a7143bce6e958209e2d5ac833ac7694a671
--- Day05/Day05.java
+++ Day05/Day05.java
@@ -77,12 +77,72 @@ public class Day05 {
     }
 
     public static void part2() throws IOException {
+        ArrayList<Stack<String>> stacks = new ArrayList<Stack<String>>();       
+        Scanner in = new Scanner (new File("input.txt"));
+
+        String line = in.nextLine();
+        int numStacks = line.length() / 4 + 1;
+
+        //initalize stacks
+        for(int i = 0; i < numStacks; i++){
+            stacks.add(new Stack<String>());
+
+            }
+
+        putStuffInStacks(line, stacks);
+
+        boolean done = false;
+
+        while(in.hasNext() && !done) {
+            line = in.nextLine();
+            if (!line.substring(1,2).equals("1")){
+                putStuffInStacks(line,stacks);
+            }
+
+            else {
+                done = true;
+            }
+
+        }
+
+        for (Stack stack : stacks){
+            Collections.reverse(stack);
+        }
+
+        while(in.hasNext()){
+            in.next();
+            int numtoMove = in.nextInt();
+            in.next();
+            int moveFrom = in.nextInt()-1;
+            in.next();
+            int moveTo = in.nextInt()-1;
+
+            Stack<String> boxes = new Stack<String>();
+
+            for(int i = 0; i < numtoMove; i++){
+                String box = stacks.get(moveFrom).pop();
+                boxes.push(box);
+
+            }
+
+            while(!boxes.isEmpty()){
+                stacks.get(moveTo).push(boxes.pop());
+            }
+        }
+
+        String result = " ";
+        for (Stack<String> stack : stacks) {
+            result += stack.pop();
+
+        }
+        System.out.println(result);
+
 
     }
 
     public static void main(String...args) {
         try {
-            part1();
+            part2();
         } catch (Exception e) {
             System.out.println(e);
         }
